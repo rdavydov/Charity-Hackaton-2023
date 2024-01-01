67c105feec997912b469a90206dac5138c886aa7
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2024-01-01 16:06:18 +0300

    relative path in js

--- src/js/GameEvents.js
+++ src/js/GameEvents.js
@@ -98,7 +98,7 @@ startGameButton.addEventListener('click', () => {
   startGameButton.innerHTML = 'Заново';
 
   flatBoard.forEach((item) => {
-    let image = item === 0 ? '' : `<img data-item="${item}" src="/photo/${boardSize}/${item}.jpg" alt="">`;
+    let image = item === 0 ? '' : `<img data-item="${item}" src="photo/${boardSize}/${item}.jpg" alt="">`;
     content.push(`<div class="cell ${className}">` + `<div class="image-wrapper">` + image + `</div>` + `</div>`);
 
     transforms[item] = [0, 0];
